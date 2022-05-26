16bfdbddc93e6fbfb8e897e0d0005c80bb6b3e63
Azamat <aabdranbayev@gmail.com>
2022-05-26 16:10:22 +0600

    cleaned dependencies

--- src/record/dto/record.dto.ts
+++ src/record/dto/record.dto.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger'
 import { IsString, IsNumber, Length } from 'class-validator'
 
 const minSize = 3
@@ -6,34 +5,23 @@ const minSize = 3
 export class CreateRecordDto {
     @IsString()
     @Length(minSize)
-    @ApiProperty({ example: 'some name', description: 'наименование записи' })
     name: string
 
     @IsString()
     @Length(minSize)
-    @ApiProperty({
-        example: 'some value',
-        description: 'Значение записи',
-    })
     value: string
 }
 
 // DTO на обновление записи
 export class UpdateRecordDto {
     @IsNumber()
-    @ApiProperty({ example: '123', description: 'айди записи' })
     id: number
 
     @IsString()
     @Length(minSize)
-    @ApiProperty({ example: 'some name', description: 'наименование записи' })
     name: string
 
     @IsString()
     @Length(minSize)
-    @ApiProperty({
-        example: 'some value',
-        description: 'Значение записи',
-    })
     value: string
 }
